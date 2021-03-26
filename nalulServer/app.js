c7d1f081228bef2b1a40c4add4879730c4df0201
Suzie Lee <wnsuwls0707@gmail.com>
2021-03-26 15:51:00 +0900

    [Fix] 연결 버그 해결

--- nalulServer/app.js
+++ nalulServer/app.js
@@ -1,8 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
+var morgan = require('morgan');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
@@ -27,7 +27,6 @@ sequelize.sync({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- nalulServer/models/index.js
+++ nalulServer/models/index.js
@@ -1,40 +1,17 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
 
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
@@ -48,3 +25,5 @@ db.Post.belongsTo(db.User,{foreignKey: 'UserIdx',targetKey:'UserIdx'});
 /* 1: N Question:Post */
 db.Question.hasMany(db.Post,{onDelete:'cascade',foreignKey: 'QuestionIdx',sourceKey:'QuestionIdx'})
 db.Post.belongsTo(db.Question,{foreignKey: 'QuestionIdx',targetKey:'QuestionIdx'})
+
+module.exports = db;
